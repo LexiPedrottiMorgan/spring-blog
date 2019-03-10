c2ef51ebf3005f719c8d743f73a150170f8c6851
Lexi Morgan <leximorgan816@gmail.com>
2019-03-10 16:09:40 -0500

    fixed navbar

--- src/main/java/com/codeup/blog/SecurityConfiguration.java
+++ src/main/java/com/codeup/blog/SecurityConfiguration.java
@@ -54,7 +54,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(
                         "/posts/create", // only authenticated users can create ads
                         "/posts/edit", // only authenticated users can edit ads
-                        "/posts/delete"
+                        "/posts/delete",
+                        "/profile"
                 )
                 .authenticated()
         ;
