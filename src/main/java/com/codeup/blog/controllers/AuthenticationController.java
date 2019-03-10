bb0e5b15641ba093187d6ffad90f4184ea889f51
Lexi Morgan <leximorgan816@gmail.com>
2019-03-10 16:53:18 -0500

    fixed file path

--- src/main/java/com/codeup/blog/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/blog/controllers/AuthenticationController.java
@@ -9,7 +9,7 @@ public class AuthenticationController {
     // login form:
     @GetMapping("/login")
     public String showLoginForm() {
-        return "/login";
+        return "user/login";
     }
 
 
